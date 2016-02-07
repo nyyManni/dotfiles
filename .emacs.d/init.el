2a416ccd22f0299e9b34a21cbc671c6398fa74da
nyyManni <henrikjohannesnyman@gmail.com>
2016-02-07 13:37:45 +0200

    Fixed perl colors in Emacs.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -17,4 +17,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(cperl-array-face ((t (:foreground "sea green" :weight bold))))
+ '(cperl-hash-face ((t (:foreground "DeepSkyBlue4"))))
+ '(cperl-nonoverridable-face ((t (:foreground "chocolate")))))
