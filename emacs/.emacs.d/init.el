9b24d116536993f1e3774d2ee9c1c495389555e6
Henrik Nyman <h@nyymanni.com>
2020-12-06 21:49:13 +0200

    Add a function to store the current frame as an .svg-file

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -205,6 +205,17 @@
   :config
   (load-theme 'gotham t))
 
+(defun screenshot-svg ()
+  "Save a screenshot of the current frame as an SVG image.
+Saves to a temp file and puts the filename in the kill ring."
+  (interactive)
+  (let* ((filename (make-temp-file "Emacs" nil ".svg"))
+         (data (x-export-frames nil 'svg)))
+    (with-temp-file filename
+      (insert data))
+    (kill-new filename)
+    (message filename)))
+
 (use-package doom-modeline
   :demand
   :init
