dcaface4038921cfa3d46358f8135427fe3f8215
Henrik Nyman <h@nyymanni.com>
2022-04-24 22:00:23 +0300

    Indicate which versions are already installed

--- fish/.config/fish/completions/mkvenv.fish
+++ fish/.config/fish/completions/mkvenv.fish
@@ -1,6 +1,12 @@
 
 function __fish_mkvenv_complete_versions
-    pyenv install --list |grep '^  [2-3]' |string trim
+    for pyversion in (pyenv install --list |grep '^  [2-3]' |string trim)
+        if [ -d  $PYENV_ROOT/versions/$pyversion ]
+            echo "$pyversion	installed"
+        else
+            echo $pyversion
+        end
+    end
 end
 
 complete -f -c mkvenv -a "(__fish_mkvenv_complete_versions)"
