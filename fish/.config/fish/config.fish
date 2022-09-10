330447621ef88a0dae171ff0085670af280a1f1b
Henrik Nyman <henrik.nyman@optofidelity.com>
2022-09-10 23:53:17 +0300

    Update shell config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -13,3 +13,5 @@ starship init fish | source
 status is-login; and pyenv init --path | source
 status is-interactive; and pyenv init - | source
 
+alias yabai-start "launchctl load ~/Library/LaunchAgents/nyymanni.selfbuilt.yabai.plist"
+alias yabai-stop "launchctl unload ~/Library/LaunchAgents/nyymanni.selfbuilt.yabai.plist"
--- fish/.config/fish/functions/mkvenv.fish
+++ fish/.config/fish/functions/mkvenv.fish
@@ -6,7 +6,7 @@ function mkvenv -d "Create virtual environment NAME with Python version VERSION"
         pyenv install $pyversion
     end
 
-    echo "==> Creating $WORKON_HOME$name..."
+    echo "==> Creating $WORKON_HOME/$name..."
     $PYENV_ROOT/versions/$pyversion/bin/python -m venv $WORKON_HOME/$name
     $WORKON_HOME/$name/bin/python -m pip install -U pip
 
