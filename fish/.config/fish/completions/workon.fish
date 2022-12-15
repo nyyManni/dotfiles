3f78792787b5de5d695e7c3cf6b8ae4db2acb524
Henrik Nyman <henrik.nyman@optofidelity.com>
2022-12-15 21:18:06 +0200

    Update fish workon completion

--- fish/.config/fish/completions/workon.fish
+++ fish/.config/fish/completions/workon.fish
@@ -1,6 +1,6 @@
 function __fish_workon_complete_venvs
     for venv in (ls -1 $WORKON_HOME)
-        echo "$venv	"(ls -l $WORKON_HOME/$venv/bin/python | sed "s|.*/versions/\(.*\)/bin/python|\1|g" |string trim)
+        echo "$venv	"(ls -l $WORKON_HOME/$venv/bin/python | sed "s|.*/[vV]ersions/\(.*\)/bin/python|\1|g" |string trim)
     end
 end
 
